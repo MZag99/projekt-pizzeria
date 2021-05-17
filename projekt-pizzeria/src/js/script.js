d0b57820ac494a7e21f8b80dc8487967ac0a72b6
Michał Zagojski <zagojski.michal.99@gmail.com>
2021-05-17 15:01:16 +0200

    Add accordion functionality

--- projekt-pizzeria/src/js/script.js
+++ projekt-pizzeria/src/js/script.js
@@ -52,7 +52,60 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id,
+      thisProduct.data = data,
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+      thisProduct.initAccordion();
+    }
+    renderInMenu(){
+      const thisProduct = this;
+
+      //generate HTML based on template
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //create element using utils.createElementFromHTML
+      thisProduct.element =  utils.createDOMFromHTML(generatedHTML);
+      //find menu container
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      //add element to menu
+      menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+      const clickableTrigger = thisProduct.element.querySelector('.product__header');
+      clickableTrigger.addEventListener('click', function(event){
+      /* prevent default action for event */
+        event.preventDefault();
+      /* find active product (product that has active class) */
+        const activeProduct = document.querySelector('.active');
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != thisProduct.element){
+          activeProduct.classList.remove('active');
+        }
+      /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data: ', thisApp.data);
+      
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +113,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
