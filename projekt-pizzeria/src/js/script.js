59e4b36a0176472d6dd5c6dd7c651edf74ef543a
Michał Zagojski <zagojski.michal.99@gmail.com>
2021-05-21 01:29:40 +0200

    Add cart toggle show/hide functionality

--- projekt-pizzeria/src/js/script.js
+++ projekt-pizzeria/src/js/script.js
@@ -205,8 +205,6 @@
     constructor(element) {
       const thisWidget = this;
 
-      console.log('AmountWidget:', thisWidget);
-      console.log('constructor arguments', element);
       thisWidget.getElements(element);
       thisWidget.setValue(settings.amountWidget.defaultValue);
       thisWidget.initActions();
@@ -259,13 +257,22 @@
 
       thisCart.products = [];
       thisCart.getElements(element);
+      thisCart.initActions();
       console.log('New Cart',thisCart);
     }
-    getElements(){
+    getElements(element){
       const thisCart = this;
 
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
@@ -278,6 +285,12 @@
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
+    },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
     },
     initData: function () {
       const thisApp = this;
@@ -294,6 +307,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
